362d16e9be803db11cba02f256965d0b0a7b1267
Plsr <christianpoplawski@gmail.com>
2024-06-30 10:53:33 +0200

    Add comment

--- src/util/http.ts
+++ src/util/http.ts
@@ -1,3 +1,8 @@
+// It would be *much* easier to just conver the buffer to
+// a string and then split in \r\n, but I want to learn,
+// so we choose the path of suffer
+//
+// TODO: This does not account for potential body data
 export function parseRequestHeaders(data: Buffer) {
   const lines = [];
   let currLine = [];
